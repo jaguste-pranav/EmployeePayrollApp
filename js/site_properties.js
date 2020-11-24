e71425e67bc783868a301afab3f7735a728505f0
jaguste.pranav <jaguste.pranav31@gmail.com>
2020-11-25 01:47:15 +0530

    [pranav] Add. Application for Employee Payroll

---
+++ js/site_properties.js
@@ -0,0 +1,7 @@
+let site_properties = {
+    use_local_storage: "false",
+    home_page: "../pages/new_home.html",  
+    add_emp_payroll_page: "../pages/newEmpPayRoll.html",
+    server_url: "http://localhost:3000/employees"   
+};
+
